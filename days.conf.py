74b8615ed31d22c55e00720bf37ab2736a574ba6
CecileLaporte <98744058+CecileLaporte@users.noreply.github.com>
2022-02-05 13:21:12 +0100

    Update days.conf.py

--- days.conf.py
+++ days.conf.py
@@ -1,16 +1,35 @@
 import datetime
 from datetime import time
 
-#jours semi-ouvrés :
+jour_ouvrables=[] 
+jour_semi_ouvrables=[]   
 
-jour_ouvrable=[]    
-
-def daysGenerator():
-    
-    openDays=[3,6]
+def daysGenerator_ouvrables():
+    openDays=[0,1,2,4]
     return(openDays)
 
-jour_ouvrable=daysGenerator()
-print(jour_ouvrable)
+def daysGenerator_semi_ouvrables():
+    semi_openDays=[2,5]
+    return(semi_openDays)
 
-time=
+jour_ouvrables=daysGenerator_ouvrables()
+jour_semi_ouvrables=daysGenerator_semi_ouvrables()
+#print(jour_ouvrables)
+#print(jour_semi_ouvrables)
+
+time=datetime.datetime.now()
+wd=time.weekday()
+#print(wd)
+
+if wd==jour_ouvrables[0] or wd==jour_ouvrables[1] or wd==jour_ouvrables[2] or wd==jour_ouvrables[3]:
+    print("jour ouvrable")
+else:
+    print("jour semi_ouvré")
+    
+jour_ouvrables[0]="lundi"
+jour_ouvrables[1]="mardi"
+jour_ouvrables[2]="jeudi"
+jour_ouvrables[3]="vendredi"
+jour_semi_ouvrables[0]="mercredi"
+jour_semi_ouvrables[1]="jeudi"
+#print(jour_ouvrables[2])
